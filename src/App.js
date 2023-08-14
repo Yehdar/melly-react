0b8567404ccc1d2dc6e0aad5984cfd7db79904f2
Yehdar <p.radhey06@gmail.com>
2023-08-14 17:27:28 -0400

    style: improve user experience

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import backgroundImage from "./assets/lol.jpeg";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
+  // if undo my changes, remove code starting here
+  const appStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    minHeight: "100vh",
+  };
+  // to here
+
+  return <div className="App" style={appStyle}></div>; //dont forget to remove the style here as well
 }
 
 export default App;
