021bf55b14c24e70c0b56582b6477d448d81a725
Melhem Fares <faresm0305@gmail.com>
2023-08-15 11:10:21 -0400

    undoing pr

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
-import backgroundImage from "./assets/lol.jpeg";
 
 function App() {
-  // if undo my changes, remove code starting here
-  const appStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center center",
-    minHeight: "100vh",
-  };
-  // to here
-
-  return <div className="App" style={appStyle}></div>; //dont forget to remove the style here as well
+  return ( 
+  <div className="App">
+    <h1>Test</h1>
+  </div>
+  )
 }
 
 export default App;
